7de4429758fcf2a7257ebbf097e761836bdd1e46
Etienne Tremel <etienne.tremel@orange.fr>
2022-06-27 22:32:51 +0200

    fix: update README.md

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Kubernetes manifests
 ## Getting started
 
 ```bash
+# bootstrap repository
 flux bootstrap github \
   --components-extra=image-reflector-controller,image-automation-controller \
   --owner=grgate \
@@ -14,4 +15,15 @@ flux bootstrap github \
   --path=clusters/kind \
   --read-write-key \
   --personal
+```
+
+Sops is used to decrypt/encrypt the GRGate private key, for new setup, use the
+following script to generate a new key:
+```bash
+# generate key
+./generate-key.sh
+
+# encrypt key used by the GRGate CLI to interact with Git commit status 
+# a GitHub app need to be created before hand, refer to GRGate documentation
+sops -e -i ./apps/bases/e2e-runner/github.private-key.sec.pem
 ```
--- generate-key.sh
+++ generate-key.sh
@@ -1,16 +1,20 @@
+#!/usr/bin/env bash
+# Generate private key and create sops-gpg secret in Kubernetes
+set -e
+
 KEY_NAME="grgate-kind"
 KEY_COMMENT="flux secrets"
 
-# gpg --batch --full-generate-key <<EOF
-# %no-protection
-# Key-Type: 1
-# Key-Length: 4096
-# Subkey-Type: 1
-# Subkey-Length: 4096
-# Expire-Date: 0
-# Name-Comment: ${KEY_COMMENT}
-# Name-Real: ${KEY_NAME}
-# EOF
+gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
 
 export KEY_FP=$(gpg --list-secret-keys "${KEY_NAME}" | head -n 2 | tail -n 1 | sed 's/\s//g')
 gpg --export-secret-keys --armor "${KEY_FP}" |
